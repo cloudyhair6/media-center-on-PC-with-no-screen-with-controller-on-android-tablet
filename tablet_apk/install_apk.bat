fe22e761834d1686e80cca0435f03708ab641c11
cloudyhair6 <familyohare5@gmail.com>
2026-06-26 17:42:04 +0100

    v1.1
    * Fixed 1 bug when joining on a browser
    * Added an updater

--- tablet_apk/install_apk.bat
+++ tablet_apk/install_apk.bat
@@ -1,13 +1,13 @@
 @echo off
-echo =========================================
-echo MiniPC Tablet APK Installer
-echo =========================================
+echo ===========================================================
+echo Media Controller (it is called Minipc) Tablet APK Installer
+echo ===========================================================
 echo Ensure your tablet is connected via USB.
 pause
 echo Installing APK...
-..\development\platform-tools\adb.exe install -r app.apk
+platform-tools\adb.exe install -r app.apk
 echo.
 echo Launching App...
-..\development\platform-tools\adb.exe shell am start -n com.minipc.kiosk/.MainActivity
+platform-tools\adb.exe shell am start -n com.minipc.kiosk/.MainActivity
 echo Done!
 pause
