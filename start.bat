5999854d2fddf8014b52ff16bbb0f5954b37b708
cloudyhair6 <familyohare5@gmail.com>
2026-06-26 20:03:27 +0100

    v1.2
    * Fixed 2 Updater bugs
    * Added in messages to the updater/installer

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
 @echo off
+echo Checking for updates...
+python installer_and_updater.py --update-check --relaunch "start.bat"
 echo [MiniPC] Starting...
 
 REM Kill explorer to remove taskbar and free up memory
@@ -6,6 +8,9 @@ taskkill /f /im explorer.exe >nul 2>&1
 
 REM Pre-compile Python for faster loading from HDD
 python -m compileall -q "%~dp0" >nul 2>&1
+
+REM Activate virtual environment if it exists
+if exist "%~dp0venv\Scripts\activate.bat" call "%~dp0venv\Scripts\activate.bat"
 
 REM Start the app
 cd /d "%~dp0pc_app"
